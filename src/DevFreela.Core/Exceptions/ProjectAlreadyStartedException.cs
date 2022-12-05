daf9c7bf1c0c3ad05f58a46d3dc9b92d6fe28104
Daniel Carlos <daniel.carlos.souza@gmail.com>
2022-12-05 14:07:11 -0300

    alteracao

--- src/DevFreela.Core/Exceptions/ProjectAlreadyStartedException.cs
+++ src/DevFreela.Core/Exceptions/ProjectAlreadyStartedException.cs
@@ -6,7 +6,7 @@ namespace DevFreela.Core.Exceptions
 {
     public class ProjectAlreadyStartedException : Exception
     {
-        public ProjectAlreadyStartedException() : base("Project is already in Started status")
+        public ProjectAlreadyStartedException() : base("Este projeto já foi iniciado")
         {
         }
     }
